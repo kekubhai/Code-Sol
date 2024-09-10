f7e1f00525b7ca0e1158f62184a79a5a43893e49
Anirban Ghosh <anirbanghosh060@gmail.com>
2024-09-10 23:56:15 +0530

    Added "StackUsingArray.C" to files.associations in settings.json

---
+++ DSA-C/Stacks/StackUsingArray.C
@@ -0,0 +1,70 @@
+
+
+
+
+
+#include <stdio.h>
+#define M 5
+
+int a[M], top=-1;
+
+void push() {
+    if (top == M-1) {
+        printf("Stack Overflow!\n");
+    } else {
+        int dd;
+        printf("Enter the element: ");
+        scanf("%d", &dd);
+        top++;
+        a[top] = dd;
+    }
+}
+
+void display() {
+    if (top == -1) {
+        printf("Stack Empty!\n");
+    } else {
+        int i;
+        for (i = 0; i <= top; i++) {
+            printf("%d ", a[i]);
+        }
+        printf("\n");
+    }
+}
+
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow!\n");
+    } else {
+        printf("%d is deleted.\n", a[top]);
+        top--;
+    }
+}
+
+int main() {
+    int ch;
+    printf("Enter 1 to Push.\n");
+    printf("Enter 2 to Pop.\n");
+    printf("Enter 3 to Display.\n");
+    printf("Enter 4 to Exit.\n");
+    
+    while (1) {
+        printf("Enter your choice: ");
+        scanf("%d", &ch);
+        switch (ch) {
+            case 1: 
+                push();
+                break;
+            case 2: 
+                pop();
+                break;
+            case 3: 
+                display();
+                break;
+            case 4: 
+                return 0;
+            default:
+                printf("Invalid choice! Try again.\n");
+        }
+    }
+}
