f09669ca71e78439992d161209c6fef893fa31d3
Anirban Ghosh <anirbanghosh060@gmail.com>
2024-09-28 22:05:04 +0530

    added more stack

---
+++ DSA-C/Stacks/StackUsingLinkedList.C
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* top = NULL;
+
+void push(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Stack overflow\n");
+        return;
+    }
+    newNode->data = value;
+    newNode->next = top;
+    top = newNode;
+}
+
+void pop() {
+    if (top == NULL) {
+        printf("Stack underflow\n");
+        return;
+    }
+    struct Node* temp = top;
+    top = top->next;
+    free(temp);
+}
+
+void peek() {
+    if (top == NULL) {
+        printf("Stack is empty\n");
+    } else {
+        printf("Top element is %d\n", top->data);
+    }
+}
+
+void display() {
+    if (top == NULL) {
+        printf("Stack is empty\n");
+    } else {
+        struct Node* temp = top;
+        while (temp != NULL) {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int choice, value;
+
+    do {
+        printf("\n1. Push\n2. Pop\n3. Peek\n4. Display\n5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to push: ");
+                scanf("%d", &value);
+                push(value);
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                peek();
+                break;
+            case 4:
+                display();
+                break;
+            case 5:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice\n");
+        }
+    } while (choice != 5);
+
+    return 0;
+}
