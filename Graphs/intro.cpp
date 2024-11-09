a14bcc411337d0f3478e1cc12799ddd9c96c7b79
Anirban Ghosh <anirbanghosh060@gmail.com>
2024-11-09 17:09:33 +0530

    added graphs

---
+++ Graphs/intro.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<unordered_map>
+#include<list>
+
+using namespace std;
+
+class graph {
+
+    unordered_map<int,list<int>> adj;
+public:
+    void addEdge(int u, int v, bool direction){
+        adj[u].push_back(v);
+
+        if(direction==0){
+            adj[v].push_back(u);
+        }
+    }
+    void printAdjList(){
+        for(auto i:adj){
+            cout<<i.first<<"-->";
+            for(auto j:i.second){
+                cout<<j<<",";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+int main(){
+    int n;
+    cout<<"Enter the number of nodes"<<endl;
+    cin>>n;
+    int m;
+    cout<<"Enter the Number of Edges"<<endl;
+    cin>>m;
+    graph g;
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        g.addEdge(u,v,0);
+    }
+    g.printAdjList();
+}
